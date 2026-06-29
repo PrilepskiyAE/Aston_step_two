e5ad07e3bc42006205c20dbac9ca4888cb8e49e9
Prilepskiy_AE <Woolf7754286@yandex.ru>
2026-06-30 00:52:55 +0700

    create base struct MyHashMap

--- src/main/java/org/prilepskiy/aston_step_two/hw_one/MyHashMap.java
+++ src/main/java/org/prilepskiy/aston_step_two/hw_one/MyHashMap.java
@@ -1,4 +1,15 @@
 package org.prilepskiy.aston_step_two.hw_one;
 
-public class MyHashMap {
+public class MyHashMap<K, V> {
+    private Node<K, V>[] table;
+    private int size;
+    private static final int DEFAULT_SIZE = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int threshold;
+
+    public MyHashMap(){
+        table = new Node[DEFAULT_SIZE];
+        threshold = (int) (DEFAULT_SIZE * LOAD_FACTOR);// == 12
+    }
+
 }
