7c064ac2aa37dd44d144f13e40a3538db32553a3
Prilepskiy_AE <Woolf7754286@yandex.ru>
2026-06-30 00:34:43 +0700

    create struct class Node

--- src/main/java/org/prilepskiy/aston_step_two/hw_one/Node.java
+++ src/main/java/org/prilepskiy/aston_step_two/hw_one/Node.java
@@ -1,4 +1,29 @@
 package org.prilepskiy.aston_step_two.hw_one;
 
-public class Node {
+import java.util.Objects;
+
+public class Node<K, V> {
+    int hash;
+    K key;
+    V value;
+    Node<K,V> next;
+
+    public Node(int hash, K key, V value, Node<K, V> next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?, ?> node = (Node<?, ?>) o;
+        return hash == node.hash && Objects.equals(key, node.key) && Objects.equals(value, node.value) && Objects.equals(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(hash, key, value);
+    }
 }
